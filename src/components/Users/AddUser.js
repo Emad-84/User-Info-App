40211985b16415acb4d6971e7d1a466c58e22e07
Emad Adas <emad.adas@yahoo.com>
2021-07-18 19:11:15 +0300

    Added Ref and Portal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,72 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
+import Wrapper from "../helper/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const userEnteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || userEnteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (none-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+userEnteredAge < 1) {
       setError({
         title: "Invalid age",
-        message: 'Please enter a valid age (> 0).'
+        message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredAge("");
-    setEnteredUsername("");
+    props.onAddUser(enteredName, userEnteredAge);
+  };
+  nameInputRef.current.value = '';
+  ageInputRef.current.value = '';
+
+  const errorHandler = () => {
+    setError(null);
   };
 
-  const errorHandler = () =>{
-    setError(null);
-  }
-
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
-      
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
-          
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
